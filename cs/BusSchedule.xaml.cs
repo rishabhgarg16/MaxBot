16101fbe11dae760c9f867b1c0d63a6a005b4afc
radbrawler <radbrawler@gmail.com>
2016-01-24 00:59:42 +0530

    integrated azure with pop up

--- cs/BusSchedule.xaml.cs
+++ cs/BusSchedule.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.WindowsAzure.MobileServices;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -27,14 +29,54 @@ namespace SDKTemplate
             this.InitializeComponent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private async void InsertData_click(object sender, RoutedEventArgs e)
+
         {
+
+            bus_timetable b1 = new bus_timetable
+
+            {
+
+                day = dayTxt.Text,
+
+                time = timeTxt.Text,
+
+                route = routeTxt.Text
+
+            };
+
+            await App.MobileService.GetTable<bus_timetable>().InsertAsync(b1);
+
+
+            var m1 = new MessageDialog("Data Inserted").ShowAsync();
+
+            dayTxt.Text = "";
+
+            timeTxt.Text = "";
+
+            routeTxt.Text = "";
 
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
+        private async void Retrive_Click(object sender, RoutedEventArgs e)
+
         {
 
+            List<bus_timetable> allPersons = await App.MobileService.GetTable<bus_timetable>().ToListAsync();
+
+            string res = "";
+
+            foreach (bus_timetable b in allPersons)
+
+            {
+
+                res += "\n\nDay :" + b.day + "\nroute :" + b.route + "\ntime :" + b.time;
+
+            }
+
+            var m1 = new MessageDialog(res).ShowAsync();
+
         }
+
     }
 }
---
+++ cs/bus_timetable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDKTemplate
+{
+    public class bus_timetable
+    {
+        public string id;
+        public string day;
+        public string route;
+        public string time;
+    }
+}
